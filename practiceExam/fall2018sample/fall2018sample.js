0546511cefb03966cf8d00df007401f024afd030
Dale <dtp1109@rit.edu>
2018-12-13 10:23:19 -0500

    new links added

--- practiceExam/fall2018sample/fall2018sample.js
+++ practiceExam/fall2018sample/fall2018sample.js
@@ -1,1 +1,12 @@
 /* Don't use <script> tags in a linked js file! */
+$(document).ready(function(){
+    $(".menuitem:first").click(function(){
+        $(".submenu:first").toggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".menuitem:last").click(function(){
+        $(".submenu:last").toggle(1000);
+    });
+});
